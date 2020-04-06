6821ff6a724a0314855c89e1659592285ffe945e
juliusabad <julius@sidekickdigital.co.uk>
2020-04-06 10:37:01 +0800

    index.php 01

--- index.php
+++ index.php
@@ -2,64 +2,50 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // include database and object files
 include_once 'api/config/database.php';
-include_once 'api/object/subject.php';
-  
+include_once 'api/object/user.php';
+
 // instantiate database and product object
 $database = new Database();
 $db = $database->getConnection();
   
 // initialize object
-$user = new Subject($db);
+$user = new User($db);
 
-$stmt = $user->read();
-$num = $stmt->rowCount();
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
 
-// check if more than 0 record found
-if($num>0){
-  
-    // products array
-    $users_arr=array();
-    $users_arr["records"]=array();
-  
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        // extract row
-        // this will make $row['name'] to
-        // just $name only
-        extract($row);
-  
-        $user_item=array(
-            "id" => $id,
-            "subject_id" => $subject_id,
-            "subject_name" => $subject_name,
-            "level" => $level,
-            "grade_year" => $grade_year,
-            "acad_year" => $acad_year,
-            "assigned_teacher" => $assigned_teacher
-        );
-  
-        array_push($users_arr["records"], $user_item);
+if(!empty($data)){
+    $user->email = $data->email;
+
+    $stmt = $user->readOne();
+    $num = $stmt->rowCount();
+
+    if($num > 0){
+        $enteredPass = md5($data->password);
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            if($enteredPass == $password){
+                http_response_code(200);
+                echo json_encode(array("code" => "Ok", "message" => "Successfully logged in", "data" => "{ id : $id, email : $email, account_type : $account_type, first_name : $first_name, last_name : $last_name }"));
+            }
+            else {
+                http_response_code(401);
+                echo json_encode(array("code" => "Error", "message" => "Invalid password"));
+            }
+        }
     }
-  
-    // set response code - 200 OK
-    http_response_code(200);
-  
-    // show products data in json format
-    echo json_encode($users_arr);
-} else{
-  
-    // set response code - 404 Not found
-    http_response_code(404);
-  
-    // tell the user no products found
-    echo json_encode(
-        array("message" => "No users found.")
-    );
 }
-
+// tell the user credentials are incomplete
+else{
+    // set response code - 400 bad request
+    http_response_code(403);
+    // tell the user
+    echo json_encode(array("message" => "Invalid username or password"));
+}
 ?>
