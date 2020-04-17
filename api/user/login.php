65aa85221a0943fb4c7cccc6d231593d6f1e2697
juliusabad <julius@sidekickdigital.co.uk>
2020-04-17 10:57:41 +0800

    added additional headers

--- api/user/login.php
+++ api/user/login.php
@@ -1,10 +1,11 @@
 <?php
 // required headers
+header('Access-Control-Allow-Credentials', true);
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Access-Control-Allow-Headers: Origin, X-Auth-Token, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 
 // include database and object files
 include_once '../config/database.php';
