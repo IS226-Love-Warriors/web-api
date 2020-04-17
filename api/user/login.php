7c61d45ca8f642db35cf71330ff334439a60d91a
juliusabad <julius@sidekickdigital.co.uk>
2020-04-17 13:56:15 +0800

    headers methods

--- api/user/login.php
+++ api/user/login.php
@@ -1,8 +1,10 @@
 <?php
 // required headers
-header("Access-Control-Allow-Origin : *");
+header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials', true);
+header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header("Access-Control-Max-Age: 3600");
 header('Access-Control-Allow-Headers: Origin, X-Auth-Token, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 header('Access-Control-Allow-Origin $http_origin');
 
