91f2e965065e2d414261a21c896438f1dd17d9bb
juliusabad <julius@sidekickdigital.co.uk>
2020-04-17 10:59:34 +0800

    added additional headers

--- api/user/login.php
+++ api/user/login.php
@@ -1,7 +1,7 @@
 <?php
 // required headers
-header('Access-Control-Allow-Credentials', true);
 header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials', true);
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
