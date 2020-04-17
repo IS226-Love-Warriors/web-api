2d935122d9dca212f793e63203993e3e42ddb7ee
juliusabad <julius@sidekickdigital.co.uk>
2020-04-17 17:19:12 +0800

    500 error code fix

--- api/user/login.php
+++ api/user/login.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials', true);
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 header('Access-Control-Allow-Origin $http_origin');
