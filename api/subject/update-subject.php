e5a27459e98e7b7af1356fed043fbe34d68d0d2b
juliusabad <julius@sidekickdigital.co.uk>
2020-05-04 23:38:22 +0800

    added new endpoint

---
+++ api/subject/update-subject.php
@@ -0,0 +1,44 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: PATCH");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// include database and object files
+include_once '../config/database.php';
+include_once '../object/subject.php';
+  
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+  
+// initialize object
+$subject = new Examination($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+if(!empty($data)){
+    $subject->subject_id = $data->subject_id;
+    $subject_stmt = $subject->readBySubjId();
+
+    while ($exam_row = $exam_stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($exam_row);
+        $exam->exam_date = (!empty($data->exam_date)) ? $data->exam_date : $exam_date;
+        $exam->exam_desc = (!empty($data->exam_desc)) ? $data->exam_desc : $exam_desc;
+        $exam->criteria_id = (!empty($data->criteria_id)) ? $data->criteria_id : $criteria_id;
+        $exam->is_active = (!empty($data->is_active)) ? 0 : $is_active;
+        $exam->examUpdate();
+    }
+
+    http_response_code(200);
+    echo json_encode(array("code" => "Ok", "message" => "Record updated"));
+}
+else{
+    // set response code - 400 bad request
+    http_response_code(403);
+    // tell the user
+    echo json_encode(array("message" => "Identifier cannot be emptied"));
+}
+?>
